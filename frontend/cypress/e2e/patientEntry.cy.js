583e270ab05432c44a9b002a98b4f523276db103
Brian308 <bahatibrianp@gmail.com>
2025-01-27 18:43:42 +0300

    Updated patientEntry E2E

--- frontend/cypress/e2e/patientEntry.cy.js
+++ frontend/cypress/e2e/patientEntry.cy.js
@@ -8,18 +8,92 @@ before("login", () => {
   loginPage = new LoginPage();
   loginPage.visit();
 });
-describe("Patient Search", function () {
-  it("User Visits Home Page and goes to Add Add|Modify Patient Page", () => {
+describe("Add/Edit Patient", function () {
+  it("User goes to Add/Edit Patient Page", () => {
     homePage = loginPage.goToHomePage();
     patientPage = homePage.goToPatientEntry();
   });
+});
+  //SEARCH PATIENT
+describe("Search for Patient", function () {
+  it("User Searches for patient by First Name", function(){
+    patientPage.clickSearchPatientBtn();
+    cy.fixture("Patient").then((patient) => {
+      patientPage.searchPatientByFirstNameOnly(patient.firstName);
+      patientPage.clickSearchBtn();
+     });
+     cy.wait(200).reload();
+    });
 
-  it("Add|Modify Patient page should appear with search field", function () {
-    patientPage
-      .getPatientEntryPageTitle()
-      .should("contain.text", "Add Or Modify Patient");
+
+  it("User Searches for patient by lastst Name", function(){
+    patientPage.clickSearchPatientBtn();
+    cy.fixture("Patient").then((patient) => {
+      patientPage.searchPatientByLastNameOnly(patient.lastName);
+      patientPage.clickSearchBtn();
+     });
+     cy.wait(200).reload();
+    });
+
+  it("Should be able to search patients By gender", function () {
+    patientPage.getMaleGenderRadioButton();
+    cy.wait(200);
+    patientPage.clickSearchBtn();
+    cy.wait(200).reload();
   });
 
+  it("Should search Patient By First and LastName", function () {
+    cy.fixture("Patient").then((patient) => {
+      patientPage.searchPatientByFirstNameOnly(patient.firstName);
+      patientPage.searchPatientByLastNameOnly(patient.lastName);
+    });
+    patientPage.clickSearchBtn();
+    cy.wait(200).reload();
+  });
+
+  it("should search patient By Date Of Birth", function () {
+    cy.fixture("Patient").then((patient) => {
+      patientPage.searchPatientByDateOfBirth(patient.DOB);
+      patientPage.clickSearchBtn();
+      patientPage.validatePatientSearchTablebyRespectiveField(
+        patient.DOB,
+        "DOB",
+      );
+    });
+    cy.wait(200).reload();
+  });
+
+  it("should search patient By Lab Number", function () {
+    cy.fixture("Patient").then((patient) => {
+      patientPage.searchPatientBylabNo(patient.labNo);
+      cy.intercept(
+        "GET",
+        `**/rest/patient-search-results?*labNumber=${patient.labNo}*`,
+      ).as("getPatientSearch");
+      patientPage.clickSearchBtn();
+      cy.wait("@getPatientSearch").then((interception) => {
+        const responseBody = interception.response.body;
+        console.log(responseBody);
+        expect(responseBody.patientSearchResults).to.be.an("array").that.is
+          .empty;
+      });
+    });
+    cy.wait(200).reload();
+  });
+
+  it("should search patient By PatientId", function () {
+    cy.fixture("Patient").then((patient) => {
+      patientPage.searchPatientByPatientId(patient.nationalId);
+      patientPage.clickSearchBtn();
+      patientPage.validatePatientSearchTable(
+        patient.firstName,
+        patient.inValidName,
+      );
+    });
+  });
+});
+  //NEW PATIENT
+describe("New Patient", function () {
   it("External search button should be deactivated", function () {
     patientPage.getExternalSearchButton();
   });
@@ -46,106 +120,7 @@ describe("Patient Search", function () {
     cy.get("div[role='status']").should("be.visible");
     cy.wait(200).reload();
   });
-
-  it("Should be able to search patients By gender", function () {
-    cy.wait(1000);
-    patientPage.getMaleGenderRadioButton().should("be.visible");
-    patientPage.getMaleGenderRadioButton().click();
-    cy.wait(200);
-    patientPage.clickSearchPatientButton();
-    cy.fixture("Patient").then((patient) => {
-      patientPage.validatePatientByGender("M");
-    });
-    cy.wait(200).reload();
-  });
-  it("Should search Patient By FirstName only", function () {
-    cy.wait(1000);
-    cy.fixture("Patient").then((patient) => {
-      patientPage.searchPatientByFirstNameOnly(patient.firstName);
-      patientPage.getFirstName().should("have.value", patient.firstName);
-      patientPage.clickSearchPatientButton();
-      patientPage.validatePatientSearchTablebyRespectiveField(
-        patient.firstName,
-        "firstName",
-      );
-    });
-    cy.wait(200).reload();
-  });
-
-  it("Should search Patient By LastName only", function () {
-    cy.wait(1000);
-    cy.fixture("Patient").then((patient) => {
-      patientPage.searchPatientByLastNameOnly(patient.lastName);
-      patientPage.getLastName().should("have.value", patient.lastName);
-      patientPage.clickSearchPatientButton();
-      patientPage.validatePatientSearchTablebyRespectiveField(
-        patient.lastName,
-        "lastName",
-      );
-    });
-    cy.wait(200).reload();
-  });
-
-  it("Should search Patient By First and LastName", function () {
-    cy.wait(1000);
-    cy.fixture("Patient").then((patient) => {
-      patientPage.searchPatientByFirstAndLastName(
-        patient.firstName,
-        patient.lastName,
-      );
-      patientPage.getFirstName().should("have.value", patient.firstName);
-      patientPage.getLastName().should("have.value", patient.lastName);
-
-      patientPage.getLastName().should("not.have.value", patient.inValidName);
-
-      patientPage.clickSearchPatientButton();
-      patientPage.validatePatientSearchTable(
-        patient.firstName,
-        patient.inValidName,
-      );
-    });
-    cy.wait(200).reload();
-  });
-  it("should search patient By Date Of Birth", function () {
-    cy.wait(1000);
-    cy.fixture("Patient").then((patient) => {
-      patientPage.searchPatientByDateOfBirth(patient.DOB);
-      patientPage.clickSearchPatientButton();
-      patientPage.validatePatientSearchTablebyRespectiveField(
-        patient.DOB,
-        "DOB",
-      );
-    });
-    cy.wait(200).reload();
-  });
-
-  it("should search patient By Lab Number", function () {
-    cy.fixture("Patient").then((patient) => {
-      patientPage.searchPatientBylabNo(patient.labNo);
-      cy.intercept(
-        "GET",
-        `**/rest/patient-search-results?*labNumber=${patient.labNo}*`,
-      ).as("getPatientSearch");
-      patientPage.clickSearchPatientButton();
-      cy.wait("@getPatientSearch").then((interception) => {
-        const responseBody = interception.response.body;
-        console.log(responseBody);
-        expect(responseBody.patientSearchResults).to.be.an("array").that.is
-          .empty;
-      });
-    });
-    cy.wait(200).reload();
-  });
-
-  it("should search patient By PatientId", function () {
-    cy.wait(1000);
-    cy.fixture("Patient").then((patient) => {
-      patientPage.searchPatientByPatientId(patient.nationalId);
-      patientPage.clickSearchPatientButton();
-      patientPage.validatePatientSearchTable(
-        patient.firstName,
-        patient.inValidName,
-      );
-    });
-  });
 });
+
+  
+
