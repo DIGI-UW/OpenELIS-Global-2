04f08f8fac8ffa4fd7f367670a2a12e98bd6e2b7
hxrshxz <harshmastic@gmail.com>
2025-02-09 21:42:17 +0530

    Cypress config file modified

--- frontend/cypress.config.js
+++ frontend/cypress.config.js
@@ -9,22 +9,7 @@ module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
-      config.specPattern = [
-        "cypress/e2e/login.cy.js",
-        "cypress/e2e/home.cy.js",
-        "cypress/e2e/patientEntry.cy.js",
-        "cypress/e2e/orderEntity.cy.js",
-        "cypress/e2e/workplan.cy.js",
-        "cypress/e2e/nonConform.cy.js",
-        "cypress/e2e/result.cy.js",
-        "cypress/e2e/validation.cy.js",
-        "cypress/e2e/modifyOrder.cy.js",
-        "cypress/e2e/report.cy.js",
-        "cypress/e2e/batchOrderEntry.cy.js",
-        "cypress/e2e/dashboard.cy.js",
-        "cypress/e2e/labNumberManagement.cy.js",
-        "cypress/e2e/AdminE2E/MenuConfig/globalMenuConfig.cy.js",
-      ];
+      config.specPattern = "cypress/e2e/**/*.cy.js";
       return config;
     },
     baseUrl: "https://localhost",
