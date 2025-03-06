f623d3abea5ec51df003c3a887f4bfed912d5d9e
Agaba Derrick <agabaderrick18@gmail.com>
2025-03-06 16:24:05 +0300

    add panelService Tests

--- src/test/java/org/openelisglobal/AppTestConfig.java
+++ src/test/java/org/openelisglobal/AppTestConfig.java
@@ -27,7 +27,8 @@ import org.openelisglobal.notification.service.TestNotificationConfigService;
 import org.openelisglobal.observationhistory.service.ObservationHistoryService;
 import org.openelisglobal.observationhistorytype.service.ObservationHistoryTypeService;
 import org.openelisglobal.organization.service.OrganizationTypeService;
-import org.openelisglobal.panel.service.PanelService;
+import org.openelisglobal.panel.dao.PanelDAO;
+import org.openelisglobal.panel.daoimpl.PanelDAOImpl;
 import org.openelisglobal.panelitem.service.PanelItemService;
 import org.openelisglobal.program.service.ImmunohistochemistrySampleService;
 import org.openelisglobal.referral.service.ReferralResultService;
@@ -89,7 +90,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
         "org.openelisglobal.program.dao", "org.openelisglobal.systemuser.daoimpl", "org.openelisglobal.note.service",
         "org.openelisglobal.requester.service", "org.openelisglobal.requester.daoimpl",
         "org.openelisglobal.organization.dao", "org.openelisglobal.note.daoimpl",
-        "org.openelisglobal.sampleorganization" }, excludeFilters = {
+        "org.openelisglobal.sampleorganization", "org.openelisglobal.panel.service",
+        "org.openelisglobal.panelitem.dao" }, excludeFilters = {
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.patient.controller.*"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.provider.controller.*.java"),
                 @ComponentScan.Filter(type = FilterType.REGEX, pattern = "org.openelisglobal.organization.controller.*"),
@@ -152,8 +154,8 @@ public class AppTestConfig implements WebMvcConfigurer {
 
     @Bean()
     @Profile("test")
-    public PanelService panelService() {
-        return mock(PanelService.class);
+    public PanelDAO panelDAO() {
+        return new PanelDAOImpl();
     }
 
     @Bean()
