be60dc2157903b331bd1fb19a91f34e5d248b421
Sarim <156024578+Saarim02@users.noreply.github.com>
2025-03-24 18:33:26 +0530

    Update PathRoute.js
    Replace window.location.href with useHistory to prevent hard refresh (This file was missed in issue #1640)

--- frontend/src/components/utils/PathRoute.js
+++ frontend/src/components/utils/PathRoute.js
@@ -1,9 +1,9 @@
+import { useHistory } from "react-router-dom";
+
 const PathRoute = ({ path, children }) => {
-  var fullPath = window.location.href;
-  if (window.location.href.includes("?")) {
-    // Remove Query Params from the path
-    fullPath = window.location.href.split("?")[0];
-  }
+  const history = useHistory();
+  let fullPath = history.location.pathname;
+
   return fullPath.endsWith(path) ? children : null;
 };
 
