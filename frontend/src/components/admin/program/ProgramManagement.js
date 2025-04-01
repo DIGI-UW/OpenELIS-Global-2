2def5a6c59a38c305c6ad2df8aff5b5341832452
Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>
2025-04-01 02:13:58 +0000

    Made program management responsive
    Signed-off-by: Vaishnavi Bhandari <vaishnavibhandari.128@gmail.com>

--- frontend/src/components/admin/program/ProgramManagement.js
+++ frontend/src/components/admin/program/ProgramManagement.js
@@ -280,6 +280,7 @@ function ProgramManagement() {
             <Column lg={16} md={8} sm={4}>
               <br></br>
             </Column>
+
             <Column lg={8} md={4} sm={4}>
               <TextArea
                 name="additionalOrderEntryQuestions"
