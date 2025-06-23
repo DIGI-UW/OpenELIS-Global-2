c94ad9c76e280955aebcee81638c414be7350295
Atwijukire Ariho Seth <120330466+Ariho-Seth@users.noreply.github.com>
2025-06-23 23:32:37 +0100

    Reflex Rule method breaking during testing

--- src/test/java/org/openelisglobal/testreflex/TestReflexServiceTest.java
+++ src/test/java/org/openelisglobal/testreflex/TestReflexServiceTest.java
@@ -1,10 +1,15 @@
 package org.openelisglobal.testreflex;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
 import org.openelisglobal.BaseWebContextSensitiveTest;
 import org.openelisglobal.testreflex.action.bean.ReflexRule;
 import org.openelisglobal.testreflex.service.TestReflexService;
+import org.openelisglobal.testreflex.valueholder.TestReflex;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TestReflexServiceTest extends BaseWebContextSensitiveTest {
@@ -18,12 +23,30 @@ public class TestReflexServiceTest extends BaseWebContextSensitiveTest {
     }
 
     @Test
+    public void testGetAllTestReflexs() {
+        List<TestReflex> testReflexs = testReflexService.getAllTestReflexs();
+        assertNotNull(testReflexs);
+        assertEquals(3, testReflexs.size());
+        assertEquals("1002", testReflexs.get(2).getId());
+
+    }
+
+    @Test
     public void testGetReflexRuleByAnalyteId() {
 
+        // There are no datasets for a Reflex rule because
+        // there's no table in the DB called Reflex Rule
+
         // This method fails to return the reflex rule as expected.
-        ReflexRule reflexRule = testReflexService.getReflexRuleByAnalyteId("3002");
+        ReflexRule reflexRule = testReflexService.getReflexRuleByAnalyteId("4002");
 
         // Assertions here;
+        assertNotNull(reflexRule);
+        assertEquals("4002", reflexRule.getId().toString());
+
+        // So it's hard to create assertions because we don't know what
+        // supposed to come from the DB!
+
     }
 
 }
