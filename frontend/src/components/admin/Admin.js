2e221b87a4498fda2f9d1fde21aa8a15c294be7d
Agaba Derrick <agabaderrick18@gmail.com>
2025-08-11 13:33:18 +0300

    fix Admin component
    Signed-off-by: Agaba Derrick <agabaderrick18@gmail.com>

--- frontend/src/components/admin/Admin.js
+++ frontend/src/components/admin/Admin.js
@@ -1,5 +1,3 @@
-"use client";
-
 import { useState, useEffect, useContext } from "react";
 import config from "../../config.json";
 import { FormattedMessage, useIntl, injectIntl } from "react-intl";
