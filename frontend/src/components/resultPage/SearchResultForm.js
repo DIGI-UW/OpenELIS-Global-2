3baaaea77a533fce1b0ac0a32eba71f5397bc4e5
Isabirye1515 <isabiryeelijah15@gmail.com>
2025-11-10 12:26:54 +0300

    Resolving the view file url in the result page

--- frontend/src/components/resultPage/SearchResultForm.js
+++ frontend/src/components/resultPage/SearchResultForm.js
@@ -875,13 +875,10 @@ export function SearchResults(props) {
   };
 
   const downloadFile = (fileName, content, fileType) => {
+    const src = `data:${fileType};base64,${content}`;
     var win = window.open();
     win.document.write(
-      '<iframe src="' +
-        fileType +
-        ";base64," +
-        content +
-        '" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>',
+      `<iframe src="${src}" frameborder="0" style="border:0; top:0px; left:0px; bottom:0px; right:0px; width:100%; height:100%;" allowfullscreen></iframe>`,
     );
   };
 
