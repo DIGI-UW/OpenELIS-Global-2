8d9105d0843de74e4d774db365f37ec31594e3a6
Piotr Mankowski <pmanko@uw.edu>
2025-12-10 22:25:24 +0000

    fix: remove unused StoragePosition import

--- src/main/java/org/openelisglobal/storage/dao/SampleStorageAssignmentDAO.java
+++ src/main/java/org/openelisglobal/storage/dao/SampleStorageAssignmentDAO.java
@@ -5,7 +5,6 @@ import java.util.Map;
 import org.openelisglobal.common.dao.BaseDAO;
 import org.openelisglobal.storage.valueholder.SampleStorageAssignment;
 import org.openelisglobal.storage.valueholder.StorageBox;
-import org.openelisglobal.storage.valueholder.StoragePosition;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 
