312dd1032ea5fe0e6d0fe1cc328e18042c0dc49c
mherman22 <hermanmuhereza22@gmail.com>
2026-01-02 12:27:10 +0300

    refactor: Remove tabs from Equipment Usage and display log directly

--- frontend/src/components/equipmentUsage/EquipmentUsageManagement.jsx
+++ frontend/src/components/equipmentUsage/EquipmentUsageManagement.jsx
@@ -1,18 +1,7 @@
-import React, { useState } from "react";
-import {
-  Tabs,
-  TabList,
-  Tab,
-  TabPanels,
-  TabPanel,
-  Grid,
-  Column,
-} from "@carbon/react";
+import { Grid, Column } from "@carbon/react";
 import { FormattedMessage } from "react-intl";
 import PageBreadCrumb from "../common/PageBreadCrumb";
 import EquipmentUsageLog from "./EquipmentUsageLog";
-import EquipmentUsageHistory from "./EquipmentUsageHistory";
-import EquipmentUsagePrintableReport from "./EquipmentUsagePrintableReport";
 import "./EquipmentUsage.css";
 
 const breadcrumbs = [
@@ -33,14 +22,9 @@ const breadcrumbs = [
  * EquipmentUsageManagement Component
  *
  * Main container for equipment (cartridge) usage tracking feature.
- * Provides tabs for:
- * - Equipment Usage Log: Record new cartridge usage
- * - Usage History: View historical usage records
- * - Reports: Generate and view usage reports
+ * Displays the Equipment Usage Log form for recording cartridge consumption.
  */
 const EquipmentUsageManagement = () => {
-  const [selectedTab, setSelectedTab] = useState(0);
-
   return (
     <>
       <PageBreadCrumb breadcrumbs={breadcrumbs} />
@@ -54,48 +38,7 @@ const EquipmentUsageManagement = () => {
               />
             </h2>
 
-            <Tabs
-              selectedIndex={selectedTab}
-              onChange={({ selectedIndex }) => setSelectedTab(selectedIndex)}
-            >
-              <TabList aria-label="Equipment usage management tabs" contained>
-                <Tab>
-                  <FormattedMessage
-                    id="equipment.usage.tab.log"
-                    defaultMessage="Equipment Usage Log"
-                  />
-                </Tab>
-                <Tab>
-                  <FormattedMessage
-                    id="equipment.usage.tab.history"
-                    defaultMessage="Usage History"
-                  />
-                </Tab>
-                <Tab>
-                  <FormattedMessage
-                    id="equipment.usage.tab.reports"
-                    defaultMessage="Reports"
-                  />
-                </Tab>
-              </TabList>
-
-              <TabPanels>
-                {/* Equipment Usage Log Tab - Record new usage */}
-                <TabPanel>
-                  <EquipmentUsageLog />
-                </TabPanel>
-
-                {/* Usage History Tab - View historical records */}
-                <TabPanel>
-                  <EquipmentUsageHistory />
-                </TabPanel>
-
-                {/* Reports Tab - Generate usage reports */}
-                <TabPanel>
-                  <EquipmentUsagePrintableReport />
-                </TabPanel>
-              </TabPanels>
-            </Tabs>
+            <EquipmentUsageLog />
           </div>
         </Column>
       </Grid>
