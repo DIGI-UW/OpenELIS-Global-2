5172de73ebfbbbdc6d1237c8323b37fc65335a53
reagan-meant <reaganmeant@gmail.com>
2026-02-05 00:24:20 +0300

    refactor(ConfigurableLinkProps): remove unnecessary line break for improved readability

--- src/test/java/org/openelisglobal/dictionary/service/DictionaryConfigurationHandlerTest.java
+++ src/test/java/org/openelisglobal/dictionary/service/DictionaryConfigurationHandlerTest.java
@@ -2,7 +2,6 @@ package org.openelisglobal.dictionary.service;
 
 import static org.junit.Assert.assertEquals;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
