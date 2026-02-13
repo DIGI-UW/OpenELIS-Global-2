d75a1c34cf58ea58222dad891ce4d4737212ec99
Agaba-derrick <agabaderrick18@gmail.com>
2026-02-13 11:20:52 +0300

    fix
    Signed-off-by: Agaba-derrick <agabaderrick18@gmail.com>

--- src/test/java/org/openelisglobal/report/controller/PatientReportRestControllerTest.java
+++ src/test/java/org/openelisglobal/report/controller/PatientReportRestControllerTest.java
@@ -4,9 +4,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.openelisglobal.BaseWebContextSensitiveTest;
-import org.openelisglobal.result.action.util.ResultsLoadUtility;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -19,25 +16,19 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 @RunWith(SpringRunner.class)
 public class PatientReportRestControllerTest extends BaseWebContextSensitiveTest {
 
-    @Autowired
-    private ApplicationContext applicationContext;
-
     @Override
     @Before
     public void setUp() throws Exception {
         super.setUp();
-
         executeDataSetWithStateManagement("testdata/patient-results-report.xml");
-
-        applicationContext.getBean(ResultsLoadUtility.class);
     }
 
     @Test
     public void getPatientResults_shouldReturnReportingDataWith13Columns_whenValidPatientIdProvided() throws Exception {
-        String patientId = "1";
 
-        mockMvc.perform(MockMvcRequestBuilders.get("/rest/reports/patient/" + patientId + "/results")
-                .contentType(MediaType.APPLICATION_JSON)).andExpect(MockMvcResultMatchers.status().isOk())
+        mockMvc.perform(
+                MockMvcRequestBuilders.get("/rest/reports/patient/1/results").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.columns").isArray())
                 .andExpect(MockMvcResultMatchers.jsonPath("$.columns.length()").value(13))
                 .andExpect(MockMvcResultMatchers.jsonPath("$.rows").isArray())
@@ -53,6 +44,7 @@ public class PatientReportRestControllerTest extends BaseWebContextSensitiveTest
 
     @Test
     public void getPatientResults_shouldReturnNotFound_whenPatientDoesNotExist() throws Exception {
+
         mockMvc.perform(
                 MockMvcRequestBuilders.get("/rest/reports/patient/999/results").contentType(MediaType.APPLICATION_JSON))
                 .andExpect(MockMvcResultMatchers.status().isNotFound());
