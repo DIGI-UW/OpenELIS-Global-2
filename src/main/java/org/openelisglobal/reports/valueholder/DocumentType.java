6111e4013b68b1d36cc0cd3697b8f7de11523512
Ketanop321 <ketanop473@gmail.com>
2026-02-17 21:20:16 +0530

    Migrate DocumentType from HBM XML to JPA annotations (#2276)
    - Add JPA annotations (@Entity, @Table, @Id, @Column, etc.) to DocumentType.java
    - Delete DocumentType.hbm.xml mapping file
    - Remove HBM mapping from hibernate.cfg.xml and test-hibernate.cfg.xml
    - Register entity in persistence.xml
    - Add Liquibase changeset to rename 'lastupdated' column to 'last_updated'
      (required for BaseObject @Column compatibility)
    
    Part of: https://github.com/DIGI-UW/OpenELIS-Global-2/issues/2276

--- src/main/java/org/openelisglobal/reports/valueholder/DocumentType.java
+++ src/main/java/org/openelisglobal/reports/valueholder/DocumentType.java
@@ -13,19 +13,43 @@
  */
 package org.openelisglobal.reports.valueholder;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import org.hibernate.annotations.DynamicUpdate;
+import org.hibernate.annotations.GenericGenerator;
+import org.hibernate.annotations.Parameter;
+import org.hibernate.annotations.Type;
 import org.openelisglobal.common.valueholder.BaseObject;
 
+@Entity
+@Table(name = "document_type")
+@DynamicUpdate
 public class DocumentType extends BaseObject<String> {
 
     private static final long serialVersionUID = 1L;
+
+    @Id
+    @Column(name = "id", precision = 10, scale = 0)
+    @GeneratedValue(generator = "document_type_seq_gen")
+    @GenericGenerator(name = "document_type_seq_gen", strategy = "org.openelisglobal.hibernate.resources.StringSequenceGenerator", parameters = @Parameter(name = "sequence_name", value = "document_type_seq"))
+    @Type(type = "org.openelisglobal.hibernate.resources.usertype.LIMSStringNumberUserType")
     private String id;
+
+    @Column(name = "name", length = 40, nullable = false)
     private String name;
+
+    @Column(name = "description", length = 80)
     private String description;
 
+    @Override
     public String getId() {
         return id;
     }
 
+    @Override
     public void setId(String id) {
         this.id = id;
     }
