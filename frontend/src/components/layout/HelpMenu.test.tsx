2f9cd228cd819424ee32d6ba8e80a07ebf71190b
Tanmay <tanmayarora382006@gmail.com>
2026-02-27 20:36:42 +0530

    prettier error

--- frontend/src/components/layout/HelpMenu.test.tsx
+++ frontend/src/components/layout/HelpMenu.test.tsx
@@ -7,62 +7,62 @@ import HelpMenu from "./HelpMenu";
 import messages from "../../languages/en.json";
 
 jest.mock("../utils/Utils", () => ({
-    getFromOpenElisServer: jest.fn(),
+  getFromOpenElisServer: jest.fn(),
 }));
 
 const { getFromOpenElisServer } = require("../utils/Utils") as {
-    getFromOpenElisServer: jest.Mock;
+  getFromOpenElisServer: jest.Mock;
 };
 
 const renderWithIntl = (component: React.ReactElement) =>
-    render(
-        <IntlProvider locale="en" messages={messages}>
-            {component}
-        </IntlProvider>,
-    );
+  render(
+    <IntlProvider locale="en" messages={messages}>
+      {component}
+    </IntlProvider>,
+  );
 
 // Tests
 
 describe("HelpMenu", () => {
-    beforeEach(() => {
-        jest.clearAllMocks();
-    });
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
 
-    test("does not crash when /rest/properties returns non-JSON (undefined)", () => {
-        getFromOpenElisServer.mockImplementation(
-            (url: string, callback: (data: undefined) => void) => {
-                if (url === "/rest/properties") {
-                    callback(undefined);
-                }
-            },
-        );
+  test("does not crash when /rest/properties returns non-JSON (undefined)", () => {
+    getFromOpenElisServer.mockImplementation(
+      (url: string, callback: (data: undefined) => void) => {
+        if (url === "/rest/properties") {
+          callback(undefined);
+        }
+      },
+    );
 
-        expect(() =>
-            renderWithIntl(
-                <HelpMenu helpOpen={false} handlePanelToggle={() => { }} />,
-            ),
-        ).not.toThrow();
-    });
+    expect(() =>
+      renderWithIntl(
+        <HelpMenu helpOpen={false} handlePanelToggle={() => {}} />,
+      ),
+    ).not.toThrow();
+  });
 
-    test("does not crash when /rest/properties returns a valid object", () => {
-        getFromOpenElisServer.mockImplementation(
-            (url: string, callback: (data: Record<string, string>) => void) => {
-                if (url === "/rest/properties") {
-                    callback({
-                        "org.openelisglobal.help.manual.url": "https://example.com/manual",
-                        "org.openelisglobal.help.tutorials.url":
-                            "https://example.com/tutorials",
-                        "org.openelisglobal.help.release-notes.url":
-                            "https://example.com/release-notes",
-                    });
-                }
-            },
-        );
+  test("does not crash when /rest/properties returns a valid object", () => {
+    getFromOpenElisServer.mockImplementation(
+      (url: string, callback: (data: Record<string, string>) => void) => {
+        if (url === "/rest/properties") {
+          callback({
+            "org.openelisglobal.help.manual.url": "https://example.com/manual",
+            "org.openelisglobal.help.tutorials.url":
+              "https://example.com/tutorials",
+            "org.openelisglobal.help.release-notes.url":
+              "https://example.com/release-notes",
+          });
+        }
+      },
+    );
 
-        expect(() =>
-            renderWithIntl(
-                <HelpMenu helpOpen={false} handlePanelToggle={() => { }} />,
-            ),
-        ).not.toThrow();
-    });
+    expect(() =>
+      renderWithIntl(
+        <HelpMenu helpOpen={false} handlePanelToggle={() => {}} />,
+      ),
+    ).not.toThrow();
+  });
 });
