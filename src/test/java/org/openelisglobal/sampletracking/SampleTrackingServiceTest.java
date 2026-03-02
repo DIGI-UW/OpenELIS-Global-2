47564ae3c10d1706db4835dbc7c953d82dda0a2b
itsmehotpants <thisisnamanagrawal@gmail.com>
2026-03-02 16:15:17 +0000

    refactor(#2401): migrate SampleTrackingServiceTest from JdbcTemplate to DBUnit

--- src/test/java/org/openelisglobal/sampletracking/SampleTrackingServiceTest.java
+++ src/test/java/org/openelisglobal/sampletracking/SampleTrackingServiceTest.java
@@ -4,15 +4,10 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import javax.sql.DataSource;
-import lombok.NonNull;
-import org.dbunit.DatabaseUnitException;
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openelisglobal.BaseWebContextSensitiveTest;
@@ -20,46 +15,25 @@ import org.openelisglobal.common.util.ConfigurationProperties;
 import org.openelisglobal.sampletracking.service.SampleTrackingService;
 import org.openelisglobal.sampletracking.valueholder.SampleTracking;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 public class SampleTrackingServiceTest extends BaseWebContextSensitiveTest {
 
     @Autowired
     private SampleTrackingService sampleTrackingService;
 
-    private JdbcTemplate jdbcTemplate;
     private List<SampleTracking> sampleTrackingList;
     private Map<String, Object> propertyValues;
     private List<String> orderProperties;
     private static int PAGE_SIZE = 0;
 
-    @Autowired
-    public void setDataSource(@NonNull DataSource dataSource) {
-        this.jdbcTemplate = new JdbcTemplate(dataSource);
-    }
-
     @Before
     public void setUp() throws Exception {
-
-        jdbcTemplate.execute("DROP TABLE IF EXISTS sampletracking CASCADE;");
-        jdbcTemplate.execute("CREATE TABLE sampletracking (accNum VARCHAR(10) PRIMARY KEY,"
-                + "PATIENTID VARCHAR(20),CLIREF VARCHAR(20),PATIENTLASTNAME VARCHAR(30),"
-                + "PATIENTFIRSTNAME VARCHAR(20),ORG_LOCAL_ABBREV VARCHAR(10),ORGNAME VARCHAR(40),"
-                + "RECDDATE VARCHAR(7),TOSID VARCHAR(10),TOSDESC VARCHAR(20),"
-                + "SOSID VARCHAR(10),SOSDESC VARCHAR(20),COLLDATE NUMERIC(7,0),"
-                + "DATEOFBIRTH NUMERIC(7,0),SORI VARCHAR(1));");
-
         executeDataSetWithStateManagement("testdata/sample-tracking.xml");
 
         propertyValues = new HashMap<>();
         propertyValues.put("sosDesc", "Scheduled");
         orderProperties = new ArrayList<>();
         orderProperties.add("patientId");
-    }
-
-    @After
-    public void cleanUp() throws SQLException, DatabaseUnitException {
-        cleanRowsInCurrentConnection(new String[] { "sampletracking" });
     }
 
     @Test
@@ -179,8 +153,7 @@ public class SampleTrackingServiceTest extends BaseWebContextSensitiveTest {
     @Test
     public void getMatchingOrderedPage_ShouldReturnAMatchingOrderedPageOfSampleTrackings_UsingAPropertyNameAndValueAndAList() {
         PAGE_SIZE = Integer.parseInt(ConfigurationProperties.getInstance().getPropertyValue("page.defaultPageSize"));
-        sampleTrackingList = sampleTrackingService.getMatchingOrderedPage("sosDesc", "Emergency", orderProperties, true,
-                1);
+        sampleTrackingList = sampleTrackingService.getMatchingOrderedPage("sosDesc", "Emergency", orderProperties, true, 1);
         assertTrue(PAGE_SIZE >= sampleTrackingList.size());
     }
 
