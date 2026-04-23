ba105fbc23303efa9a5855cae110f203cfa93344
Sagar Khandagre <sagar.khandagre998@gmail.com>
2026-04-23 12:25:20 +0530

    apply formating

--- frontend/playwright/tests/foundational/core/patient-study-edit.spec.ts
+++ frontend/playwright/tests/foundational/core/patient-study-edit.spec.ts
@@ -1,5 +1,5 @@
 import { test, expect } from "@playwright/test";
-import { Page, Route } from '@playwright/test';
+import { Page, Route } from "@playwright/test";
 import {
   UI_TIMEOUT,
   SHORT_TIMEOUT,
