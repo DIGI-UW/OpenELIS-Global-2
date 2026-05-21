d774c91979247dd31e7e774cb4e6d3f2130d7be8
reagan-meant <reaganmeant@gmail.com>
2026-05-21 17:40:17 +0300

    fix: Remove unused imports in LoggingController for cleaner code

--- src/main/java/org/openelisglobal/logging/controller/LoggingController.java
+++ src/main/java/org/openelisglobal/logging/controller/LoggingController.java
@@ -32,8 +32,6 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Component;
-import org.openelisglobal.common.log.LogEvent;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
