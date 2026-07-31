1f0cbacf0b580d8f79ab30aa2d8dc7530f618819
Ian <ian.c.bacher@gmail.com>
2026-07-30 18:22:48 -0700

    Remove negative test

--- src/test/java/org/openelisglobal/scratchinv/ScratchInheritedHandlerController.java
+++
@@ -1,8 +0,0 @@
-package org.openelisglobal.scratchinv;
-
-import org.springframework.web.bind.annotation.RestController;
-
-// no class-level @RequestMapping, no @PreAuthorize; handler inherited from ScratchBase
-@RestController
-public class ScratchInheritedHandlerController extends ScratchBase {
-}
