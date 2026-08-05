55f86094211761e940ec56146c85931c02c50682
Piotr Mankowski <pmanko@uw.edu>
2026-08-04 22:02:31 -0700

    test(text macros): isolate ORM validation

--- src/test/java/org/openelisglobal/textmacro/TextMacroOrmValidationTest.java
+++ src/test/java/org/openelisglobal/textmacro/TextMacroOrmValidationTest.java
@@ -2,20 +2,43 @@ package org.openelisglobal.textmacro;
 
 import static org.junit.Assert.assertNotNull;
 
-import jakarta.persistence.EntityManagerFactory;
+import org.hibernate.SessionFactory;
+import org.hibernate.boot.registry.StandardServiceRegistry;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.openelisglobal.BaseWebContextSensitiveTest;
 import org.openelisglobal.textmacro.valueholder.TextMacro;
-import org.springframework.beans.factory.annotation.Autowired;
 
-public class TextMacroOrmValidationTest extends BaseWebContextSensitiveTest {
+public class TextMacroOrmValidationTest {
 
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
+    private static StandardServiceRegistry registry;
+    private static SessionFactory sessionFactory;
+
+    @BeforeClass
+    public static void buildSessionFactory() {
+        Configuration configuration = new Configuration();
+        configuration.addAnnotatedClass(TextMacro.class);
+        configuration.setProperty("hibernate.dialect", "org.hibernate.dialect.PostgreSQLDialect");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "none");
+        registry = new StandardServiceRegistryBuilder().applySettings(configuration.getProperties()).build();
+        sessionFactory = configuration.buildSessionFactory(registry);
+    }
+
+    @AfterClass
+    public static void closeSessionFactory() {
+        if (sessionFactory != null) {
+            sessionFactory.close();
+        }
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
 
     @Test
     public void textMacroMappingIsRegistered() {
-        assertNotNull(entityManagerFactory.getMetamodel().entity(TextMacro.class));
-        assertNotNull(entityManagerFactory.getMetamodel().entity(TextMacro.class).getAttribute("contexts"));
+        assertNotNull(sessionFactory.getMetamodel().entity(TextMacro.class));
+        assertNotNull(sessionFactory.getMetamodel().entity(TextMacro.class).getAttribute("contexts"));
     }
 }
