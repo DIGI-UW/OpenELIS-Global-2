eb957fd58c4a3358f0cfe9bd9085a392b666a99f
Piotr Mankowski <pmanko@uw.edu>
2026-08-22 01:27:57 -0700

    test(OGC-782): isolate analyzer ingress security

--- src/test/java/org/openelisglobal/common/util/ControllerUtillsTest.java
+++ src/test/java/org/openelisglobal/common/util/ControllerUtillsTest.java
@@ -2,11 +2,13 @@ package org.openelisglobal.common.util;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertSame;
 
 import org.junit.Test;
 import org.openelisglobal.common.action.IActionConstants;
 import org.openelisglobal.login.valueholder.UserSessionData;
 import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpSession;
 
 public class ControllerUtillsTest {
 
@@ -20,5 +22,18 @@ public class ControllerUtillsTest {
         assertNull(request.getSession(false));
         assertEquals("42", ControllerUtills.getSysUserId(request));
         assertNull("Reading the audit actor must not create a session", request.getSession(false));
+    }
+
+    @Test
+    public void existingBrowserSessionUserDataIsResolvedWithoutReplacingTheSession() {
+        MockHttpServletRequest request = new MockHttpServletRequest();
+        MockHttpSession session = new MockHttpSession();
+        UserSessionData user = new UserSessionData();
+        user.setSytemUserId(43);
+        session.setAttribute(IActionConstants.USER_SESSION_DATA, user);
+        request.setSession(session);
+
+        assertEquals("43", ControllerUtills.getSysUserId(request));
+        assertSame(session, request.getSession(false));
     }
 }
